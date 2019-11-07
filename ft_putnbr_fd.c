908fffd2d1a4850c3690d23f509a478aab5d94a8
Valentin Lageard <valentin.lageard.pro@gmail.com>
2019-11-07 20:22:29 +0100

    write functions added

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlageard <vlageard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 17:28:12 by vlageard          #+#    #+#             */
+/*   Updated: 2019/11/07 17:34:15 by vlageard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int nbr;
+
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = (unsigned int)(n * -1);
+	}
+	else
+		nbr = (unsigned int)n;
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr/10, fd);
+	ft_putchar_fd((char)nbr % 10 + '0', fd);
+}
